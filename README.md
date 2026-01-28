662e54c7ed755f151434f77edef27385000d7329
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-01-28 22:15:07 +0100

    f

--- README.md
+++ README.md
@@ -110,11 +110,7 @@ Ensure your target module function accepts a session (packref) as its first para
 ```tsx
 import { blobs, modules, packref, packs, sha256, textref, texts } from "@hazae41/stdbob"
 
-export namespace addresses {
-
-  export function compute(session: packref): textref {
-    return blobs.toBase16(sha256.digest(blobs.encode(session)))
-  }
+namespace addresses {
 
   export function verify(session: packref): textref {
     const module = packs.get<textref>(session, 0)
@@ -122,7 +118,10 @@ export namespace addresses {
     if (!packs.get<bool>(modules.call(module, texts.fromString("verify"), packs.create1(session)), 0))
       throw new Error("Invalid session")
 
-    return compute(session)
+    if (packs.length(session) === 1)
+      return module
+
+    return blobs.toBase16(sha256.digest(blobs.encode(session)))
   }
 
 }
--- tst/token/src/libs/address/mod.ts
+++ tst/token/src/libs/address/mod.ts
@@ -2,33 +2,16 @@ import { blobs, modules, packref, packs, sha256, textref, texts } from "@hazae41
 
 export namespace addresses {
 
-  /**
-   * Compute an address from a session
-   * @param module 
-   * @param caller 
-   * @returns address
-   */
-  export function compute(session: packref): textref {
-    const module = packs.get<textref>(session, 0)
-
-    if (packs.length(session) === 1)
-      return module
-
-    return blobs.toBase16(sha256.digest(blobs.encode(session)))
-  }
-
-  /**
-   * Verify a session against forgery and return its address
-   * @param session 
-   * @returns address
-   */
   export function verify(session: packref): textref {
     const module = packs.get<textref>(session, 0)
 
     if (!packs.get<bool>(modules.call(module, texts.fromString("verify"), packs.create1(session)), 0))
       throw new Error("Invalid session")
 
-    return compute(session)
+    if (packs.length(session) === 1)
+      return module
+
+    return blobs.toBase16(sha256.digest(blobs.encode(session)))
   }
 
 }
